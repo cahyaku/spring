1b32d71439cefcf511da8e710f36170718d8fd2a
cahya kumala <cahyakumala573@gmail.com>
2025-08-04 11:50:49 +0800

    config properties

---
+++ spring-config-properties/src/test/java/com/cahya/spring/config/SpringConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cahya.spring.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
